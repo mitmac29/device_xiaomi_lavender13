0bc69abb9d254f1b38f99cb42aa2f4226298642b
DanipunK1 <pegate.eltrueco@gmail.com>
2023-01-15 23:54:51 +0100

    lavender: Add support for V4A

--- bootleg_lavender.mk
+++ bootleg_lavender.mk
@@ -8,6 +8,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# ViPER4Android FX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Inherit some common Bootleggers ROM stuff
 $(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
